551e01831b58e733c57b53f4fb78b2ec31cd8772
SebastianSosaUPM <Sebastian.Sosa@alumnos.upm.es>
2025-04-10 19:18:15 +0200

    Issue#2_1

--- src/main/java/com/practica/genericas/Coordenada.java
+++ src/main/java/com/practica/genericas/Coordenada.java
@@ -34,10 +34,10 @@ public class Coordenada {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		final int PRIME = 31;
 		int result = 1;
-		result = prime * result + Float.floatToIntBits(latitud);
-		result = prime * result + Float.floatToIntBits(longitud);
+		result = PRIME * result + Float.floatToIntBits(latitud);
+		result = PRIME * result + Float.floatToIntBits(longitud);
 		return result;
 	}
 
@@ -45,16 +45,14 @@ public class Coordenada {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Coordenada other = (Coordenada) obj;
-		if (Float.floatToIntBits(latitud) != Float.floatToIntBits(other.latitud))
-			return false;
-		if (Float.floatToIntBits(longitud) != Float.floatToIntBits(other.longitud))
-			return false;
-		return true;
+		else {
+			if (obj == null || getClass() != obj.getClass())
+				return false;
+			Coordenada other = (Coordenada) obj;
+			if (Float.floatToIntBits(latitud) != Float.floatToIntBits(other.latitud) || Float.floatToIntBits(longitud) != Float.floatToIntBits(other.longitud))
+				return false;
+			return true;
+		}
 	}
 	
 }
